447c4c06404423572d902d301bc8a3e53cbf8979
Kerolosnady1 <kerolosnady444@gmail.com>
2026-09-19 21:30:01 +0300

    update: the structure to add apply_success.html and job_detail.html

--- event_platform/event_platform/urls.py
+++ event_platform/event_platform/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
 """
-'''
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,20 +22,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
 ]
-''' # OLD CODE
-
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
-    path('events/', include('events.urls')),
-    path('jobs/', include('job_board.urls')), # Added Job Board
-]
-
-# This allows Django to serve uploaded files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- job_board_platform/job_board/admin.py
+++ job_board_platform/job_board/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Employer, Job, Application
 
 # Register your models here.
+admin.site.register(Employer)
+admin.site.register(Job)
+admin.site.register(Application)
+
